9b8daa9705df93ccb34f46fa1339e565145c446c
unknown <>
2018-03-29 11:48:02 +0800

    ReactDemo

---
+++ src/components/table/MyTable.js
@@ -0,0 +1,93 @@
+/**
+ * @authors jiayzh
+ * @date    2018/03/26
+ * @description 表格组件
+ */
+
+import React from 'react'
+import { Table, Icon } from 'antd'
+
+// 表格组件
+export default class myTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tDate: [],
+            selectedRowKeys: []
+        }
+    }
+
+    componentDidMount () {
+        const data = []
+
+        for (let i = 0; i < 50; i++) {
+            data.push({
+                key: i,
+                name: `编号${i}`,
+                age: 26,
+                address: `上海市张衡路${i}号`,
+                remark: 'http://www.baidu.com',
+                operate: '暂无'
+            })
+        }
+
+        this.setState({
+            tDate: data
+        })
+    }
+
+    // checkbox状态
+    onSelectChange = (selectedRowKeys) => {
+        console.log('selectedRowKeys changed: ', selectedRowKeys)
+        this.setState({ selectedRowKeys })
+    }
+
+    render () {
+        const columns = [{
+            title: '姓名',
+            width: '20%',
+            dataIndex: 'name'
+        }, {
+            title: '年龄',
+            width: '20%',
+            dataIndex: 'age',
+        }, {
+            title: '住址',
+            width: '20%',
+            dataIndex: 'address'
+        }, {
+            title: '备注',
+            width: '20%',
+            dataIndex: 'remark',
+            render (text) {
+                return <a href={text} target="_blank">百度</a>
+            }
+        }, {
+            title: '操作',
+            width: '20%',
+            dataIndex: 'operate'
+        }]
+
+        const { selectedRowKeys } = this.state
+
+        const rowSelection = {
+            selectedRowKeys,
+            onChange: this.onSelectChange
+        }
+
+        const pagination = {
+            total: this.state.tDate.length,
+            showSizeChanger: true,
+            onShowSizeChange (current, pageSize) {
+                console.log('Current: ', current, '; PageSize: ', pageSize)
+            },
+            onChange (current) {
+                console.log('Current: ', current)
+            }
+        }
+
+        return (
+            <Table rowSelection={rowSelection} columns={columns} dataSource={this.state.tDate} bordered pagination={pagination} />
+        )
+    }
+}
