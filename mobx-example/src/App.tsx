3e8068cc79618f131700f23b3f416f38991ab78f
408705279.com <408705279@qq.com>
2018-05-29 14:56:52 +0800

    refresh project tree

--- mobx-example/src/App.tsx
+++ mobx-example/src/App.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
-import "./App.css";
 
 class App extends React.Component {
     public render(): JSX.Element {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">To get started, edit <code>src/App.tsx</code> and save to reload.</p>
-            </div>
+            <div></div>
         );
     }
 }
