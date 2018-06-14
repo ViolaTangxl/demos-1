e4503194246c31cda37c555f29abba0d49974a55
war408705279 <408705279@qq.com>
2018-06-14 14:41:00 +0800

    Modify page1 and page2

--- vue-example/src/components/PageOne.vue
+++ vue-example/src/components/PageOne.vue
@@ -1,10 +1,28 @@
 <template>
-    <div class="page-one"></div>
+    <div class="page-one">
+        <div class="title-container">这是page1</div>
+        <div class="count-container">总数: {{this.count}}</div>
+        <div class="btn-container">
+            <el-button type="success" plain v-on:click="addOne">加一(同步)</el-button>
+            <el-button type="danger" plain v-on:click="reduceOne">减一(同步)</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "PageOne"
+  name: "PageOne",
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    addOne: function() {
+      this.count++;
+    },
+    reduceOne: function() {
+      this.count--;
+    }
+  }
 };
 </script>
 
@@ -12,6 +30,27 @@ export default {
 .page-one {
   width: 100%;
   height: 100%;
-  background-color: #ddd;
+}
+.page-one .title-container {
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  font-size: 16px;
+  text-align: center;
+}
+.page-one .count-container {
+  width: 100%;
+  height: 30px;
+  position: relative;
+  top: calc(50% - 30px);
+  line-height: 30px;
+  font-size: 16px;
+  text-align: center;
+}
+.page-one .btn-container {
+  width: 100%;
+  position: relative;
+  top: calc(50% - 20px);
+  text-align: center;
 }
 </style>
--- vue-example/src/components/PageTwo.vue
+++ vue-example/src/components/PageTwo.vue
@@ -1,10 +1,28 @@
 <template>
-    <div class="page-two"></div>
+    <div class="page-two">
+        <div class="title-container">这是page2</div>
+        <div class="count-container">总数: {{this.count}}</div>
+        <div class="btn-container">
+            <el-button type="success" plain v-on:click="addOne">加一(同步)</el-button>
+            <el-button type="danger" plain v-on:click="reduceOne">减一(同步)</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "PageTwo"
+  name: "PageTwo",
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    addOne: function() {
+      this.count++;
+    },
+    reduceOne: function() {
+      this.count--;
+    }
+  }
 };
 </script>
 
@@ -12,6 +30,27 @@ export default {
 .page-two {
   width: 100%;
   height: 100%;
-  background-color: #ddd;
+}
+.page-two .title-container {
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  font-size: 16px;
+  text-align: center;
+}
+.page-two .count-container {
+  width: 100%;
+  height: 30px;
+  position: relative;
+  top: calc(50% - 30px);
+  line-height: 30px;
+  font-size: 16px;
+  text-align: center;
+}
+.page-two .btn-container {
+  width: 100%;
+  position: relative;
+  top: calc(50% - 20px);
+  text-align: center;
 }
 </style>
