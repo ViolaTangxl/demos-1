7d0b7bdd9ef95a599d28cb45ba082473ebd44bf9
war408705279 <408705279@qq.com>
2018-06-19 11:38:14 +0800

    Add buttons to elements page

--- vue-example/src/components/Elements.vue
+++ vue-example/src/components/Elements.vue
@@ -3,6 +3,37 @@
        v-bind:style="wsStyle"
        v-loading="loading">
     <div class="title-container">这是page5</div>
+    <div class="elements-container">
+      <!-- 按钮 -->
+      <div class="element-item">
+        <div class="item-title">按钮:</div>
+        <div class="item-container">
+          <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary"
+                       plain>主要按钮</el-button>
+            <el-button type="success"
+                       round>成功按钮</el-button>
+            <el-button type="info"
+                       icon="el-icon-message"
+                       circle></el-button>
+            <el-button type="warning"
+                       disabled>警告按钮（禁用）</el-button>
+            <el-button type="text">文字按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+            <el-button-group>
+              <el-button type="primary"
+                         icon="el-icon-arrow-left">上一页</el-button>
+              <el-button type="primary">下一页
+                <i class="el-icon-arrow-right"></i>
+              </el-button>
+            </el-button-group>
+            <el-button type="success"
+                       v-bind:loading="loadingBtn.isLoading">{{loadingBtn.text}}</el-button>
+          </el-row>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +46,10 @@ export default {
   data: () => ({
     count: 0,
     loading: false,
+    loadingBtn: {
+      isLoading: false,
+      text: "加载按钮"
+    },
     wsStyle: waterPrint({
       username: "jiayizhen page5"
     })
@@ -31,13 +66,37 @@ export default {
 .elements-page {
   width: 100%;
   height: 100%;
-  overflow: hidden;
 }
+
 .elements-page .title-container {
   width: 100%;
   height: 30px;
   line-height: 30px;
   font-size: 16px;
   text-align: center;
+}
+
+.elements-page .elements-container {
+  width: 100%;
+  height: calc(100% - 30px);
+}
+
+.elements-page .elements-container .element-item {
+  width: 96%;
+  position: relative;
+  left: 2%;
+  margin-top: 10px;
+}
+
+.elements-page .element-item .item-title {
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  font-size: 16px;
+}
+
+.elements-page .element-item .item-container {
+  width: 100%;
+  position: relative;
 }
 </style>
