9311b7e941a7dcf4e0dfb4e3cda395ded87a8708
war408705279 <408705279@qq.com>
2018-06-22 17:07:44 +0800

    Add water print to page

--- vux-example/src/components/VuxPageOne.vue
+++ vux-example/src/components/VuxPageOne.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>VuxPageOne</div>
+  <div class="vux-page-one"
+       v-bind:style="wsStyle"></div>
 </template>
 
 <script>
+// 引入水印插件
+import { waterPrint } from "../assets/plugins/waterprint.js";
+
 export default {
   name: "VuxPageOne",
   data() {
-    return {};
+    return {
+      wsStyle: waterPrint({
+        username: "jiayizhen vux1"
+      })
+    };
   }
 };
 </script>
 
 <style>
+.vux-page-one {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 </style>
--- vux-example/src/components/VuxPageTwo.vue
+++ vux-example/src/components/VuxPageTwo.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>VuxPageTwo</div>
+  <div class="vux-page-two"
+       v-bind:style="wsStyle"></div>
 </template>
 
 <script>
+// 引入水印插件
+import { waterPrint } from "../assets/plugins/waterprint.js";
+
 export default {
   name: "VuxPageTwo",
   data() {
-    return {};
+    return {
+      wsStyle: waterPrint({
+        username: "jiayizhen vux2"
+      })
+    };
   }
 };
 </script>
 
 <style>
+.vux-page-two {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
 </style>
