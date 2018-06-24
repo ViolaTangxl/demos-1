88c8153e8fe987248e28ec9568c881f99bbb6b00
war408705279 <408705279@qq.com>
2018-06-24 17:55:56 +0800

    Add go home page btn

--- vuex-example/src/layout/Layout.vue
+++ vuex-example/src/layout/Layout.vue
@@ -8,10 +8,15 @@
     <el-container class="right-part">
       <el-header class="right-header"
                  height="56px">
-        <span class="header-btn"
+        <span class="collapse-btn"
               v-on:click="collapseMenu">
           <i v-bind:class="isCollapse ? 'el-icon-arrow-right' : 'el-icon-arrow-left'"></i>
           <span>{{ isCollapse ? "展开" : "收起" }}</span>
+        </span>
+        <span class="home-btn"
+              title="首页"
+              v-on:click="goHomePage">
+          <i class="el-icon-date"></i>
         </span>
       </el-header>
       <el-main class="right-body">
@@ -43,6 +48,9 @@ export default {
   methods: {
     collapseMenu: function() {
       this.isCollapse = !this.isCollapse;
+    },
+    goHomePage: function() {
+      this.$router.push({ path: "/" });
     }
   },
   components: {
@@ -74,27 +82,37 @@ export default {
   background-color: #545c64;
 }
 
-.layout .right-header .header-btn {
+.layout .right-header .collapse-btn {
   position: relative;
   top: calc(50% - 10px);
   left: 10px;
   color: #fff;
   font-size: 18px;
   cursor: pointer;
-  display: inline-block;
 }
 
-.layout .right-header .header-btn:hover {
+.layout .right-header .collapse-btn:hover,
+.layout .right-header .home-btn:hover {
   color: #ffd04b;
 }
 
-.layout .header-btn > i {
+.layout .collapse-btn > i,
+.layout .home-btn > i {
   font-size: 18px;
 }
 
-.layout .header-btn > span {
+.layout .collapse-btn > span {
   position: relative;
   top: -1px;
+}
+
+.layout .right-header .home-btn {
+  position: relative;
+  top: calc(50% - 10px);
+  left: calc(100% - 80px);
+  color: #fff;
+  font-size: 18px;
+  cursor: pointer;
 }
 
 .layout .right-part .right-body {
