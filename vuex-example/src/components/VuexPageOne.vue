a607273f5474bcf3a738058b7963a5d4b7ded2eb
war408705279 <408705279@qq.com>
2018-06-25 09:34:54 +0800

    Add scoped attribute to style label

--- vuex-example/src/components/VuexPageOne.vue
+++ vuex-example/src/components/VuexPageOne.vue
@@ -50,7 +50,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .vuex-page-one {
   width: 100%;
   height: 100%;
--- vuex-example/src/page/HomePage.vue
+++ vuex-example/src/page/HomePage.vue
@@ -17,7 +17,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .home-page {
   width: 100%;
   height: 100%;
