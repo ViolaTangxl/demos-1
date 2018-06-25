eebf704407b0eb27ccf8fcdee0de017f96f3b927
war408705279 <408705279@qq.com>
2018-06-25 09:50:11 +0800

    Refresh project tree and add shortcut icon

--- user-manage/src/Enter.vue
+++ user-manage/src/Enter.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "Enter"
 };
 </script>
 
--- user-manage/src/main.js
+++ user-manage/src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Enter from './Enter'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#main',
   router,
   components: {
-    App
+    Enter
   },
-  template: '<App/>'
+  template: '<Enter />'
 })
