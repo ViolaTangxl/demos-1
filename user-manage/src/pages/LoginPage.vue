f469a84b5cbd55f09ca8d5e4732895f12a021801
war408705279 <408705279@qq.com>
2018-06-26 09:10:13 +0800

    Add user name and pass word check

--- user-manage/src/pages/LoginPage.vue
+++ user-manage/src/pages/LoginPage.vue
@@ -4,25 +4,26 @@
       <div class="login-img">
         <img src="@/assets/images/github.jpg" />
       </div>
-      <el-form v-bind:model="formModel"
+      <el-form v-bind:model="loginForm"
                v-bind:rules="rules"
                status-icon
                size="small"
-               ref="formModel"
+               ref="loginForm"
                class="login-form">
         <el-form-item label="用户名"
                       prop="username">
-          <el-input v-model="formModel.username"></el-input>
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
         <el-form-item label="密码"
                       prop="password">
           <el-input type="password"
-                    v-model="formModel.password"
+                    v-model="loginForm.password"
                     auto-complete="off"></el-input>
         </el-form-item>
         <el-form-item class="form-btn">
-          <el-button type="primary">确定</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary"
+                     v-on:click="confirm('loginForm')">确定</el-button>
+          <el-button v-on:click="reset('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -34,7 +35,7 @@ export default {
   name: "LoginPage",
   data: function() {
     return {
-      formModel: {
+      loginForm: {
         username: "",
         password: ""
       },
@@ -45,6 +46,38 @@ export default {
         password: [{ required: true, message: "请输入密码", trigger: "blur" }]
       }
     };
+  },
+  methods: {
+    confirm: function(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          const { username, password } = this.loginForm;
+          if (username.trim() !== "jiayizhen") {
+            this.showMessage("error", "用户名错误", 1500);
+            return false;
+          }
+          if (password.trim() !== "jiayizhen") {
+            this.showMessage("error", "密码错误", 1500);
+            return false;
+          }
+          this.showMessage("success", "登录成功", 1500);
+        } else {
+          return false;
+        }
+      });
+    },
+    reset: function(formName) {
+      this.$refs[formName].resetFields();
+    },
+    showMessage: function(type, message, duration) {
+      this.$message({
+        type: type,
+        message: message,
+        center: true,
+        duration: duration,
+        onClose: true
+      });
+    }
   }
 };
 </script>
