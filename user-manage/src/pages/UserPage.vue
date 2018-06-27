12a5c8dfa840281a8ccc66767c34f4440962304a
war408705279 <408705279@qq.com>
2018-06-27 12:04:40 +0800

    Add autocomplete component to user info table

--- user-manage/src/pages/UserPage.vue
+++ user-manage/src/pages/UserPage.vue
@@ -2,7 +2,17 @@
   <div class="user-page">
     <div class="head-container">
       <span class="head-title">用户信息表</span>
-      <div class="head-control"></div>
+      <div class="head-control">
+        <!-- 控制器 -->
+        <!-- 栅格布局 -->
+        <el-row :gutter="10">
+          <el-col :span="6">
+            <!-- 搜索 -->
+            <el-autocomplete class="user-search-input"
+                             placeholder="搜索用户"></el-autocomplete>
+          </el-col>
+        </el-row>
+      </div>
     </div>
     <div class="table-container">
       <UserTable v-bind:userData="userData"
