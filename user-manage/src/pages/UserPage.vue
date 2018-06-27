b6f063652f674e622c567474a98dd704924ad0b5
war408705279 <408705279@qq.com>
2018-06-27 09:07:37 +0800

    Modify user page

--- user-manage/src/pages/UserPage.vue
+++ user-manage/src/pages/UserPage.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="user-page">
+    <div class="head-container">
+      <span class="head-title">用户信息表</span>
+      <div class="head-control"></div>
+    </div>
+    <div class="table-container"></div>
+  </div>
 </template>
 
 <script>
