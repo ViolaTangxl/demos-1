01acf8d8ead3fd07e9dbfcb4c7c5074b246b051d
war408705279 <408705279@qq.com>
2018-07-10 09:13:55 +0800

    Update custom page

--- user-manage/src/pages/CustomPage.vue
+++ user-manage/src/pages/CustomPage.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="custom-page">
+    <div class="head-container">
+      <span class="head-title">自定义主题</span>
+      <div class="head-control">
+        <!-- 控制器 -->
+        <!-- 栅格布局 -->
+        <el-row :gutter="10">
+          <el-col :span="24"
+                  class="control-btns">
+            <!-- 重置按钮 -->
+            <el-button>重置</el-button>
+            <!-- 确定按钮 -->
+            <el-button type="primary">确定</el-button>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="theme-container">
+    </div>
   </div>
 </template>
 
