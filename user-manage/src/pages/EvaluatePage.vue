91cf17e68024101f54a3081f11d81ba1c72ff7d1
war408705279 <408705279@qq.com>
2018-07-15 11:42:58 +0800

    Update evaluate page

--- user-manage/src/pages/EvaluatePage.vue
+++ user-manage/src/pages/EvaluatePage.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="evaluate-page">
+  <div class="evaluate-page"
+       v-loading="pageLoad"
+       element-loading-text="玩儿命加载中"
+       element-loading-spinner="el-icon-loading"
+       element-loading-background="rgba(0, 0, 0, 0.6)">
+    <div class="head-container">
+      <span class="head-title">反馈页面</span>
+      <div class="head-control">
+        <!-- 控制器 -->
+        <!-- 栅格布局 -->
+        <el-row :gutter="10">
+          <el-col :span="24"
+                  class="control-btns">
+            <!-- 重置按钮 -->
+            <el-button>重置</el-button>
+            <!-- 确定按钮 -->
+            <el-button type="primary">提交</el-button>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="main-container">
+    </div>
   </div>
 </template>
 
@@ -14,7 +36,9 @@
 export default {
   name: "EvaluatePage",
   data: function() {
-    return {};
+    return {
+      pageLoad: false
+    };
   }
 };
 </script>
