a7ca6ba171f6274a1d000ff0e7581ceb01b33e1e
war408705279 <408705279@qq.com>
2018-07-16 09:07:08 +0800

    Update form in evaluate page

--- user-manage/src/pages/EvaluatePage.vue
+++ user-manage/src/pages/EvaluatePage.vue
@@ -26,69 +26,32 @@
                v-bind:rules="formRules"
                ref="evaluateForm"
                label-width="100px">
-        <el-form-item label="活动名称"
+        <el-form-item label="姓名"
                       prop="name">
           <el-input v-model="evaluateForm.name"></el-input>
         </el-form-item>
-        <el-form-item label="活动区域"
-                      prop="region">
-          <el-select v-model="evaluateForm.region"
-                     placeholder="请选择活动区域">
-            <el-option label="区域一"
-                       value="shanghai"></el-option>
-            <el-option label="区域二"
-                       value="beijing"></el-option>
+        <el-form-item label="邮箱"
+                      prop="email">
+          <el-input v-model="evaluateForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="住址"
+                      prop="address">
+          <el-input v-model="evaluateForm.address"></el-input>
+        </el-form-item>
+        <el-form-item label="性别"
+                      prop="sex">
+          <el-select v-model="evaluateForm.sex"
+                     placeholder="请选择性别">
+            <el-option label="男"
+                       value="male"></el-option>
+            <el-option label="女"
+                       value="female"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="活动时间"
-                      required>
-          <el-col :span="11">
-            <el-form-item prop="date1">
-              <el-date-picker type="date"
-                              placeholder="选择日期"
-                              v-model="evaluateForm.date1"
-                              style="width: 100%;"></el-date-picker>
-            </el-form-item>
-          </el-col>
-          <el-col class="line"
-                  :span="2">-</el-col>
-          <el-col :span="11">
-            <el-form-item prop="date2">
-              <el-time-picker type="fixed-time"
-                              placeholder="选择时间"
-                              v-model="evaluateForm.date2"
-                              style="width: 100%;"></el-time-picker>
-            </el-form-item>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="即时配送"
-                      prop="delivery">
-          <el-switch v-model="evaluateForm.delivery"></el-switch>
-        </el-form-item>
-        <el-form-item label="活动性质"
-                      prop="type">
-          <el-checkbox-group v-model="evaluateForm.type">
-            <el-checkbox label="美食/餐厅线上活动"
-                         name="type"></el-checkbox>
-            <el-checkbox label="地推活动"
-                         name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动"
-                         name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光"
-                         name="type"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源"
-                      prop="resource">
-          <el-radio-group v-model="evaluateForm.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式"
-                      prop="desc">
+        <el-form-item label="反馈信息"
+                      prop="content">
           <el-input type="textarea"
-                    v-model="evaluateForm.desc"></el-input>
+                    v-model="evaluateForm.content"></el-input>
         </el-form-item>
       </el-form>
     </div>
@@ -110,50 +73,26 @@ export default {
       pageLoad: false,
       evaluateForm: {
         name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        email: "",
+        address: "",
+        sex: "",
+        content: ""
       },
       formRules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
-        ],
-        region: [
-          { required: true, message: "请选择活动区域", trigger: "change" }
-        ],
-        date1: [
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
           {
-            type: "date",
-            required: true,
-            message: "请选择日期",
-            trigger: "change"
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
           }
         ],
-        date2: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择时间",
-            trigger: "change"
-          }
-        ],
-        type: [
-          {
-            type: "array",
-            required: true,
-            message: "请至少选择一个活动性质",
-            trigger: "change"
-          }
-        ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
+        address: [{ required: true, message: "请输入地址", trigger: "blur" }],
+        sex: [{ required: true, message: "请选择性别", trigger: "change" }],
+        content: [
+          { required: true, message: "请输入反馈信息", trigger: "blur" }
+        ]
       }
     };
   }
