8375095fd7b5be544ba8c0234dbb0a2beb0eb81f
war408705279 <408705279@qq.com>
2021-07-09 17:43:18 +0800

    修改 blog index 页面

--- next/pages/blog/index.tsx
+++ next/pages/blog/index.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import Layout from 'components/Layout'
 
-export default function Blog() {
+export default function BlogIndexPage() {
+
   return (
     <Layout
-      title="博客页面"
-      description="博客页面"
-      keywords="博客页面"
+      title="博客首页"
+      description="博客首页"
+      keywords="博客首页"
     >
-      博客页面
+      博客首页
     </Layout>
   )
 }
