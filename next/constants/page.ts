1a964205544fba8f5b6b357a5a410acb307fcaf5
war408705279 <408705279@qq.com>
2021-07-16 18:41:59 +0800

    添加页面路由相关常量

--- next/constants/page.ts
+++ next/constants/page.ts
@@ -4,3 +4,15 @@
 
 export const defaultTitle = '七牛问答'
 export const titleSuffix = ' - 七牛问答'
+
+export enum Page {
+  Index = 'index',
+  Blog = 'blog',
+  NotFound = 'notFound'
+}
+
+export const pageRoutes = {
+  [Page.Index]: '/',
+  [Page.Blog]: '/blog',
+  [Page.NotFound]: '/404'
+}
