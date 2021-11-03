84d926fd5afdb3f716aa42391f23d585e7dfee1a
war408705279 <408705279@qq.com>
2021-11-03 16:23:07 +0800

    添加 Overview 页面组件

---
+++ kratos/src/components/Overview/index.tsx
@@ -0,0 +1,28 @@
+/**
+ * @file component Overview
+ * @author jiayizhen <jiayizhen@qiniu.com>
+ */
+
+import React from 'react'
+
+import Page from 'components/Page'
+
+import IconWJDLNB from './images/icon-wjdlnb.jpg'
+
+import styles from './style.m.less'
+
+export default function Overview() {
+  return (
+    <Page
+      className={styles.main}
+      title="概览"
+    >
+      <img
+        className={styles.img}
+        src={IconWJDLNB}
+        title="维军大佬牛逼"
+        alt="维军大佬牛逼"
+      />
+    </Page>
+  )
+}
