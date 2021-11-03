82fee49ad5ee4db1fe368c8c6fa245447b1ace92
war408705279 <408705279@qq.com>
2021-11-03 15:44:07 +0800

    公用颜色定义修改

--- kratos/src/utils/styles/colors/ts/grey.ts
+++ kratos/src/utils/styles/colors/ts/grey.ts
@@ -7,13 +7,15 @@
  */
 
 enum Grey {
-  grey1 = '#FFFFFF', // 模块背景
-  grey2 = '#F5F5F5', // 默认背景，界面背景
-  grey3 = '#EEEEEE', // 分割线、占位符
-  grey4 = '#BFBFBF', // 暗文本
-  grey5 = '#999999', // 副标题、描述文本
-  grey6 = '#666666', // 正文
-  grey7 = '#333333' // 主标题、正文
+  grey1 = '#FFFFFF',
+  grey2 = '#F5F5F5',
+  grey3 = '#EEEEEE',
+  grey4 = '#BFBFBF',
+  grey5 = '#999999',
+  grey6 = '#666666',
+  grey7 = '#333333',
+  white = grey1,
+  black = '#000'
 }
 
 export default Grey
