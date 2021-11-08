95ea22357bacb4ace60906fd4a2880a618ec13c2
war408705279 <408705279@qq.com>
2021-11-08 15:32:52 +0800

    添加 member && birthday 常量文件

---
+++ kratos/src/constants/birthday.ts
@@ -0,0 +1,29 @@
+/**
+ * @file birthday-relative constants
+ * @author jiayizhen <jiayizhen@qiniu.com>
+ */
+
+import { MemberName } from 'constants/member'
+
+export const BirthdayMap = {
+  [MemberName.SheWeiJun]: {
+    month: 5,
+    date: 20
+  },
+  [MemberName.ZhangHuiJuan]: {
+    month: 11,
+    date: 30
+  },
+  [MemberName.LiuCaiFeng]: {
+    month: 7,
+    date: 18
+  },
+  [MemberName.WangMeiWang]: {
+    month: 10,
+    date: 30
+  },
+  [MemberName.TangXueLei]: {
+    month: 10,
+    date: 9
+  }
+}
---
+++ kratos/src/constants/member.ts
@@ -0,0 +1,28 @@
+/**
+ * @file member-relative constants
+ * @author jiayizhen <jiayizhen@qiniu.com>
+ */
+
+export enum MemberName {
+  SheWeiJun = 'sheweijun',
+  ZhangHuiJuan = 'zhanghuijuan',
+  LiuCaiFeng = 'liucaifeng',
+  WangMeiWang = 'wangmeiwang',
+  TangXueLei = 'tangxuelei'
+}
+
+export const MemberNameTextMap = {
+  [MemberName.SheWeiJun]: '维军大佬',
+  [MemberName.ZhangHuiJuan]: '百神',
+  [MemberName.LiuCaiFeng]: '彩凤大佬',
+  [MemberName.WangMeiWang]: '美旺大佬',
+  [MemberName.TangXueLei]: '唐大佬'
+}
+
+export const MemberNameList = [
+  MemberName.SheWeiJun,
+  MemberName.ZhangHuiJuan,
+  MemberName.LiuCaiFeng,
+  MemberName.WangMeiWang,
+  MemberName.TangXueLei
+]
