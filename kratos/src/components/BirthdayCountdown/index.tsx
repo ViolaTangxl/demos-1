b015a7cc463213df2ffebe4364a37943b25ce090
war408705279 <408705279@qq.com>
2021-11-08 15:34:14 +0800

    修改 BirthdayCountdown 页面组件渲染方式

--- kratos/src/components/BirthdayCountdown/index.tsx
+++ kratos/src/components/BirthdayCountdown/index.tsx
@@ -9,15 +9,46 @@ import { observer } from 'mobx-react'
 import Page from 'components/Page'
 import FlipDown from 'components/FlipDown'
 
+import {
+  MemberNameTextMap,
+  MemberNameList
+} from 'constants/member'
+import { BirthdayMap } from 'constants/birthday'
+
 import styles from './style.m.less'
 
 export default observer(function BirthdayCountdown() {
+  function renderFlipDowns() {
+    return MemberNameList.map(
+      (name: string, index: number) => {
+        const matchBirthday = BirthdayMap[name]
+        const { month, date } = matchBirthday
+
+        return (
+          <div
+            key={index}
+            className={styles.flipDownWrapper}
+          >
+            <div className={styles.tip}>
+              距离{MemberNameTextMap[name]}的生日还有
+            </div>
+            <FlipDown
+              className={styles.flipDown}
+              month={month}
+              date={date}
+            />
+          </div>
+        )
+      }
+    )
+  }
+
   return (
     <Page
       className={styles.main}
       title="生日倒计时"
     >
-      <FlipDown />
+      {renderFlipDowns()}
     </Page>
   )
 })
