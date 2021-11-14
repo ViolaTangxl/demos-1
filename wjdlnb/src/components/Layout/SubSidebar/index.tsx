9c0cf60a0acee26922e29831365af71748db2460
war408705279 <408705279@qq.com>
2021-11-14 17:48:03 +0800

    修改左侧导航栏中导航项的 z-index 为 1

--- wjdlnb/src/components/Layout/SubSidebar/index.tsx
+++ wjdlnb/src/components/Layout/SubSidebar/index.tsx
@@ -3,7 +3,7 @@
  * @author: jiayizhen <jiayizhen@qiniu.com>
  */
 
-import React from 'react'
+import React, { CSSProperties } from 'react'
 
 import BaseSubSidebar, { LinkItem } from 'portal-base/common/components/SubSidebar'
 
@@ -11,10 +11,26 @@ import { name } from 'constants/app'
 import { basename } from 'constants/route'
 
 export default function SubSidebar() {
+  // Overview 组件页面中的 Fireworks 组件 z-index 为 0
+  // 将左边栏导航项的 z-index 设置为 1 是为了保证导航栏能够点击
+  const linkItemStyle: CSSProperties = {
+    zIndex: 1
+  }
+
   return (
     <BaseSubSidebar title={name}>
-      <LinkItem to={`${basename}/overview`}>概览</LinkItem>
-      <LinkItem to={`${basename}/birthday`}>生日倒计时</LinkItem>
+      <LinkItem
+        style={linkItemStyle}
+        to={`${basename}/overview`}
+      >
+        概览
+      </LinkItem>
+      <LinkItem
+        style={linkItemStyle}
+        to={`${basename}/birthday`}
+      >
+        生日倒计时
+      </LinkItem>
     </BaseSubSidebar>
   )
 }
